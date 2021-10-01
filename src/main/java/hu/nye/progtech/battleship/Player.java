0491dc249df26001ad02356f5242ac786d187a4b
Bor Bálint <br.blnt10@gmail.com>
2021-10-01 12:04:41 +0200

    Update files

--- src/main/java/hu/nye/progtech/battleship/Player.java
+++ src/main/java/hu/nye/progtech/battleship/Player.java
@@ -1,9 +1,13 @@
 package hu.nye.progtech.battleship;
 
+import java.util.ArrayList;
+
 public class Player {
     private String Name;
     private Board board;
     private int numberOfShips;
+    private ArrayList<Ship> ships;
+
 
 
 
