a1fb25623574611ef4298214b5a5a44770fe2652
Bor Bálint <br.blnt10@gmail.com>
2021-11-14 13:02:59 +0100

    sql load

--- database.sql
+++ database.sql
@@ -1,11 +1,16 @@
+--          Create table                    --
 CREATE TABLE game_saves (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
     wins int NOT NULL
 );
-
+--              Query                       --
 SELECT * FROM game_saves WHERE name = ;
+
 SELECT * FROM game_saves;
+
 UPDATE game_saves SET wins = WHERE name = ;
+
 DELETE FROM game_saves WHERE name = ;
+
 INSERT INTO game_saves (name, wins) VALUES (name, wins);
--- src/main/java/hu/nye/progtech/battleship/persistance/impl/SavePlayersToRepositoryJDBC.java
+++ src/main/java/hu/nye/progtech/battleship/persistance/impl/SavePlayersToRepositoryJDBC.java
@@ -1,10 +1,12 @@
 package hu.nye.progtech.battleship.persistance.impl;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
+import hu.nye.progtech.battleship.model.Board;
 import hu.nye.progtech.battleship.model.Player;
 import hu.nye.progtech.battleship.persistance.SavePlayersToRepository;
+import hu.nye.progtech.battleship.service.properties.ConfigReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +24,8 @@ public class SavePlayersToRepositoryJDBC implements SavePlayersToRepository, Aut
     private static final String UPDATE_STATEMENT = "UPDATE game_saves SET wins = ? WHERE name = ?;";
 
     private Connection connection;
+    private ResultSet resultSet;
+    private Statement statement;
 
     private List<Player> players;
 
@@ -42,7 +46,28 @@ public class SavePlayersToRepositoryJDBC implements SavePlayersToRepository, Aut
     }
 
     @Override
-    public List<Player> load() {
+    public List<Player> load()  {
+        try {
+            loadAll();
+            return players;
+        } catch (SQLException throwables) {
+            LOGGER.warn(throwables + "sql error while select all");
+        }
         return null;
+    }
+
+    private void loadAll() throws SQLException{
+         statement = connection.createStatement();
+         resultSet = statement.executeQuery(SELALL_STATEMENT);
+        while (resultSet.next()) {
+            String name = resultSet.getString("NAME");
+            int win = Integer.parseInt(resultSet.getString("WINS"));
+            Player temp = new Player(new Board(
+                    Integer.parseInt(ConfigReader.getPropertyFromConfig("board.setting.board.size"))),
+                    Integer.parseInt(ConfigReader.getPropertyFromConfig("board.setting.numberofships")));
+            temp.setName(name);
+            temp.setNumberOfWins(win);
+            players.add(temp);
+        }
     }
 }
